6d171acc25080e1a4a3fad11b4655fabc316be57
Renato Gentil <112764390+rrdog-aws@users.noreply.github.com>
2025-09-11 09:16:49 +0100

    tshoot codedeploy stage

--- php-fpm/index.php
+++ php-fpm/index.php
@@ -145,7 +145,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['purchase'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Try here ---- Car Insurance</title>
+    <title>Try here -AAAAAA-- Car Insurance</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
